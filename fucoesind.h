4f583b1ecf8f11f75bf8201e57f390a8a3821026
Jonathas Art's <jonathasarts@gmail.com>
2013-10-21 20:44:18 -0200

    Create fucoesind.h

---
+++ fucoesind.h
@@ -0,0 +1,7 @@
+
+void mensagemValido(void);
+int isNumeric (const char* str);
+void cabecalho(void);
+void comandocerto(int *i, int *option, int *tokenint);
+void executandocomandos (int *comando, int *opcao, int *valor);
+void sairdoSistema(void);
